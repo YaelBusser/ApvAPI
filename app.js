0ff32f543084e45172824b0cc1e9049054150017
Yaël Busser <yael.busser@gmail.com>
2024-06-04 23:06:00 +0200

    revert

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(bodyParser.json());
 
 app.use(express.static('public'));
 
-// Connexion à la base de donnéese
+// Connexion à la base de données
 (async () => {
     try {
         await sequelize.authenticate();
